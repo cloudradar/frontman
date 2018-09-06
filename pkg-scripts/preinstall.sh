ffb998e9333b7fea88025eb20d1401241e4ab5ba
requilence <requilence@gmail.com>
2018-09-06 14:09:48 +0300

    dep/rpm: create /usr/lib/sysctl.d if not exists

--- pkg-scripts/preinstall.sh
+++ pkg-scripts/preinstall.sh
@@ -10,5 +10,6 @@ if [ -z `getent passwd frontman` ]; then
   useradd  --gid frontman --system --shell /bin/false frontman
 fi
 
+mkdir -p /usr/lib/sysctl.d
 sysctl -w net.ipv4.ping_group_range="0   2147483647"
 echo "net.ipv4.ping_group_range = 0 2147483647" > /usr/lib/sysctl.d/50-ping_group_range.conf
