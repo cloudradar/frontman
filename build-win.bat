c93116a64738c10aae3cd99ee5c7718afdb3908a
felix <felix@psy-coding.com>
2018-09-25 12:22:49 +0200

    Setup msi building and signin on remote VM

--- build-win.bat
+++ build-win.bat
@@ -1,10 +1,18 @@
-CD /D "%~dp0"
-FOR /F "tokens=* USEBACKQ" %%F IN (`git describe --always --tag`) DO (
-SET frontman_version=%%F
-)
-COPY dist\windows_386\frontman.exe frontman.exe
-go-msi.exe make --src pkg-scripts\msi-templates --msi dist/frontman_%frontman_version%_Windows_i386.msi --version %frontman_version% --arch 386
+SET build_dir=C:\Users\hero\frontman_ci\build_msi\%1
+SET frontman_version=%2
+SET cert_pass=%3
+
+SET PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin;c:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x86;C:\Program Files\go-msi
+CD %build_dir%
+
+
+COPY dist\frontman_386.exe frontman.exe
+go-msi make --src pkg-scripts\msi-templates --msi dist/frontman_32.msi --version %frontman_version% --arch 386
 DEL frontman.exe
-COPY dist\windows_amd64\frontman.exe frontman.exe
-go-msi.exe make --src pkg-scripts\msi-templates --msi dist/frontman_%frontman_version%_Windows_x86_64.msi --version %frontman_version% --arch amd64
+
+COPY dist\frontman_64.exe frontman.exe
+go-msi make --src pkg-scripts\msi-templates --msi dist/frontman_64.msi --version %frontman_version% --arch amd64
 DEL frontman.exe
+
+signtool sign /t http://timestamp.digicert.com /f "C:\Users\hero\frontman_ci/build_msi/cloudradar.io.p12" /p %cert_pass% dist/frontman_32.msi
+signtool sign /t http://timestamp.digicert.com /f "C:\Users\hero\frontman_ci/build_msi/cloudradar.io.p12" /p %cert_pass% dist/frontman_64.msi
