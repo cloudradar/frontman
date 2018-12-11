543fb74cc8b2306e7254a8f4f9f510e22e81abee
felix <felix@psy-coding.com>
2018-12-11 08:56:35 +0800

    try to get around integrity level by copying files

--- build-win.bat
+++ build-win.bat
@@ -7,12 +7,15 @@ CD %build_dir%
 
 
 COPY dist\frontman_386.exe frontman.exe
-go-msi make --src pkg-scripts\msi-templates --msi dist/frontman_32.msi --version %frontman_version% --arch 386
+go-msi make --src pkg-scripts\msi-templates --msi dist/_frontman_32.msi --version %frontman_version% --arch 386
 DEL frontman.exe
 
 COPY dist\frontman_64.exe frontman.exe
-go-msi make --src pkg-scripts\msi-templates --msi dist/frontman_64.msi --version %frontman_version% --arch amd64
+go-msi make --src pkg-scripts\msi-templates --msi dist/_frontman_64.msi --version %frontman_version% --arch amd64
 DEL frontman.exe
+
+COPY dist/_frontman_32.msi dist/frontman32.msi
+COPY dist/_frontman_64.msi dist/frontman64.msi
 
 ::signtool sign /t http://timestamp.comodoca.com /f "C:\Users\hero\frontman_ci\build_msi\cloudradar.io.p12" /p %cert_pass% dist/frontman_32.msi
 ::signtool sign /t http://timestamp.comodoca.com /f "C:\Users\hero\frontman_ci\build_msi\cloudradar.io.p12" /p %cert_pass% dist/frontman_64.msi
