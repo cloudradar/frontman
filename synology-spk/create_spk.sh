ff7593107f8c6bd625c0105c3b5c2366b74ba234
Martin Lindhe <martin@ubique.se>
2019-05-30 14:17:59 +0200

    circleci: attempt fix spk release

--- synology-spk/create_spk.sh
+++ synology-spk/create_spk.sh
@@ -15,10 +15,10 @@ GOOS=linux GOARCH=arm GOARM=7 go build github.com/cloudradar-monitoring/frontman
 mv -f frontman 1_create_package/frontman
 
 cd 1_create_package
-tar cfz package.tgz *
+tar cvfz package.tgz *
 mv package.tgz ../2_create_project/
 cd ../2_create_project/
-tar cfz frontman.spk *
+tar cvfz frontman.spk *
 mv frontman.spk ../frontman-armv7.spk
 rm -f package.tgz
 cd ..
@@ -28,10 +28,10 @@ GOOS=linux GOARCH=arm64 go build github.com/cloudradar-monitoring/frontman/cmd/f
 mv -f frontman 1_create_package/frontman
 
 cd 1_create_package
-tar cfz package.tgz *
+tar cvfz package.tgz *
 mv package.tgz ../2_create_project/
 cd ../2_create_project/
-tar cfz frontman.spk *
+tar cvfz frontman.spk *
 mv frontman.spk ../frontman-armv8.spk
 rm -f package.tgz
 cd ..
@@ -41,10 +41,10 @@ GOOS=linux GOARCH=amd64 go build github.com/cloudradar-monitoring/frontman/cmd/f
 mv -f frontman 1_create_package/frontman
 
 cd 1_create_package
-tar cfz package.tgz *
+tar cvfz package.tgz *
 mv package.tgz ../2_create_project/
 cd ../2_create_project/
-tar cfz frontman.spk *
+tar cvfz frontman.spk *
 mv frontman.spk ../frontman-amd64.spk
 rm -f package.tgz
 cd ..
