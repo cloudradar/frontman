1eae89f83a2cefdddf5a1851eb51aabab9c0af73
Martin Lindhe <martin@ubique.se>
2019-07-04 19:03:36 +0200

    spk: ensure binaries are statically linked with CGO_ENABLED=0

--- synology-spk/create_spk.sh
+++ synology-spk/create_spk.sh
@@ -12,7 +12,7 @@ rm 2_create_project/INFO.bak
 sed -i.bak "s/{PKG_ARCH}/noarch/g" 2_create_project/INFO
 rm 2_create_project/INFO.bak
 
-GOOS=linux GOARCH=arm GOARM=7 go build github.com/cloudradar-monitoring/frontman/cmd/frontman/...
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build github.com/cloudradar-monitoring/frontman/cmd/frontman/...
 mv -f frontman 1_create_package/frontman
 
 cd 1_create_package
@@ -34,7 +34,7 @@ rm 2_create_project/INFO.bak
 sed -i.bak "s/{PKG_ARCH}/noarch/g" 2_create_project/INFO
 rm 2_create_project/INFO.bak
 
-GOOS=linux GOARCH=arm64 go build github.com/cloudradar-monitoring/frontman/cmd/frontman/...
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build github.com/cloudradar-monitoring/frontman/cmd/frontman/...
 mv -f frontman 1_create_package/frontman
 
 cd 1_create_package
@@ -56,7 +56,7 @@ rm 2_create_project/INFO.bak
 sed -i.bak "s/{PKG_ARCH}/x86_64 cedarview bromolow broadwell/g" 2_create_project/INFO
 rm 2_create_project/INFO.bak
 
-GOOS=linux GOARCH=amd64 go build github.com/cloudradar-monitoring/frontman/cmd/frontman/...
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build github.com/cloudradar-monitoring/frontman/cmd/frontman/...
 mv -f frontman 1_create_package/frontman
 
 cd 1_create_package
