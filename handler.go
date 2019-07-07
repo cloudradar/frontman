90cca1bb3686f6b6f2009d30e9c9991350d15294
Martin Lindhe <martin@ubique.se>
2019-07-07 12:00:00 +0200

    handler: remove 'too many open files' check since root cause was resolved in #98

--- handler.go
+++ handler.go
@@ -207,14 +207,6 @@ func (fm *Frontman) InputFromHub() (*Input, error) {
 
 func (fm *Frontman) PostResultsToHub(results []Result) error {
 	fm.initHubClient()
-	for _, res := range results {
-		if s, ok := res.Message.(string); ok {
-			if strings.Contains(s, "too many open files") {
-				// NOTE: work-around for too many open files. Remove this when resolved
-				return fmt.Errorf("skipping post to hub because of %v", res.Message)
-			}
-		}
-	}
 
 	fm.offlineResultsBuffer = append(fm.offlineResultsBuffer, results...)
 	b, err := json.Marshal(Results{
