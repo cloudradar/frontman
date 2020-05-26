8d94a1878462abc37cdb2d50ef342082fb717c4a
Nikita Vanyasin <nikita.vanyasin@gmail.com>
2020-05-26 23:46:06 +0300

    Rework SSL checks implementation to fix issues and catch more invalid certificates
    Old implementation wasn't checking for CA certificates validity. Also it didn't support certificates with multiple chains supplied.
    New tests added to check for bad and good certificates.

---
+++ ssl_test.go
@@ -0,0 +1,102 @@
+// +build !quick_tests
+
+package frontman
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFrontman_runSSLCheck(t *testing.T) {
+	badSSL := []string{
+		"expired.badssl.com",
+		"wrong.host.badssl.com",
+		"self-signed.badssl.com",
+		"untrusted-root.badssl.com",
+
+		"sha1-intermediate.badssl.com",
+
+		// cipher suite
+		"rc4-md5.badssl.com",
+		"rc4.badssl.com",
+		"null.badssl.com",
+
+		// key exchange
+		"dh480.badssl.com",
+		"dh512.badssl.com",
+		"dh1024.badssl.com",
+		"dh2048.badssl.com",
+		"dh-small-subgroup.badssl.com",
+		"dh-composite.badssl.com",
+
+		// certificate transparency
+		"invalid-expected-sct.badssl.com",
+
+		// upgrade
+		"subdomain.preloaded-hsts.badssl.com",
+
+		// known bad
+		"Superfish.badssl.com",
+		"eDellRoot.badssl.com",
+		"DSDTestProvider.badssl.com",
+		"preact-cli.badssl.com",
+		"webpack-dev-server.badssl.com",
+
+		// chrome tests
+		"captive-portal.badssl.com",
+		"mitm-software.badssl.com",
+
+		// defunct
+		"sha1-2017.badssl.com",
+	}
+
+	goodSSL := []string{
+		"badssl.com",
+		"sha256.badssl.com",
+		"sha384.badssl.com",
+		"sha512.badssl.com",
+
+		"1000-sans.badssl.com",
+		"ecc256.badssl.com",
+		"ecc384.badssl.com",
+		"rsa2048.badssl.com",
+		"rsa4096.badssl.com",
+		"extended-validation.badssl.com",
+		"client.badssl.com",
+		"mozilla-modern.badssl.com",
+
+		"hsts.badssl.com",
+		"upgrade.badssl.com",
+		"preloaded-hsts.badssl.com",
+		"https-everywhere.badssl.com",
+
+		"long-extended-subdomain-name-containing-many-letters-and-dashes.badssl.com",
+		"longextendedsubdomainnamewithoutdashesinordertotestwordwrapping.badssl.com",
+	}
+
+	if testing.Short() {
+		badSSL = badSSL[:2]
+		goodSSL = goodSSL[:2]
+	}
+
+	cfg, _ := HandleAllConfigSetup(DefaultCfgPath)
+	fm := helperCreateFrontman(t, cfg)
+
+	for _, badSSLHost := range badSSL {
+		ipaddr, resolveErr := resolveIPAddrWithTimeout(badSSLHost, timeoutDNSResolve * 10)
+		assert.NoError(t, resolveErr)
+
+		_, err := fm.runSSLCheck(&net.TCPAddr{IP: ipaddr.IP, Port: 443}, badSSLHost, "https")
+		assert.Error(t, err, badSSLHost)
+	}
+
+	for _, goodSSLHost := range goodSSL {
+		ipaddr, resolveErr := resolveIPAddrWithTimeout(goodSSLHost, timeoutDNSResolve * 10)
+		assert.NoError(t, resolveErr)
+
+		_, err := fm.runSSLCheck(&net.TCPAddr{IP: ipaddr.IP, Port: 443}, goodSSLHost, "https")
+		assert.NoError(t, err, goodSSLHost)
+	}
+}
