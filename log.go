20a9a67c949021293abcee1bc182b0f48d35206d
Martin Lindhe <martin@ubique.se>
2020-10-23 15:53:07 +0200

    log: make frontman.stats writing thread safe

--- log.go
+++ log.go
@@ -59,11 +59,9 @@ func addLogFileHook(file string, flag int, chmod os.FileMode) error {
 	return nil
 }
 
-func addErrorHook(stats *stats.FrontmanStats) {
+func (fm *Frontman) addErrorHook() {
 	hook := &LogrusErrorHook{
-		InternalErrorsTotal:        &stats.InternalErrorsTotal,
-		InternalLastErrorMessage:   &stats.InternalLastErrorMessage,
-		InternalLastErrorTimestamp: &stats.InternalLastErrorTimestamp,
+		fm: fm,
 	}
 
 	logrus.AddHook(hook)
@@ -149,17 +147,18 @@ func (fm *Frontman) SetLogLevel(lvl LogLevel) {
 }
 
 type LogrusErrorHook struct {
-	InternalErrorsTotal        *uint64
-	InternalLastErrorMessage   *string
-	InternalLastErrorTimestamp *uint64
+	fm *Frontman
 }
 
 func (h *LogrusErrorHook) Fire(entry *logrus.Entry) error {
 	now := uint64(time.Now().Unix())
 
-	*h.InternalErrorsTotal++
-	*h.InternalLastErrorMessage = entry.Message
-	*h.InternalLastErrorTimestamp = now
+	h.fm.statsLock.Lock()
+	defer h.fm.statsLock.Unlock()
+
+	h.fm.stats.InternalErrorsTotal++
+	h.fm.stats.InternalLastErrorMessage = entry.Message
+	h.fm.stats.InternalLastErrorTimestamp = now
 
 	return nil
 }
@@ -199,7 +198,7 @@ func (fm *Frontman) configureLogger() {
 
 	// Add hook to logrus that updates our LastInternalError statistics
 	// whenever an error log is done
-	addErrorHook(fm.stats)
+	fm.addErrorHook()
 
 	// sets standard logging to /dev/null
 	devNull, err := os.OpenFile(os.DevNull, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
